c7706f4e7ad6d76e24296e764eb6d34b159017e3
Olivier Larchevêque <olivier.larcheveque@auf.org>
2012-09-19 12:34:40 -0400

    1.8

--- auf/django/saml/admin.py
+++ auf/django/saml/admin.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from views import redirect_to_login
+from views import redirect_to_login, redirect_to_logout
 from django.contrib.admin import site
 
 def saml_login(request, extra_context=None):
     return redirect_to_login(request)
 
+def saml_logout(request, extra_context=None):
+    return redirect_to_logout(request)
 
-# on recable runtime la fonction qui prend en charge
-# le login côté de l'admin pour avoir le même comportement
-# que dans le frontend
+# on recable runtime les fonctions qui prennent en charge
+# l'authentification pour communiquer avec l'IdP
 site.login = saml_login
+site.logout = saml_logout
 
