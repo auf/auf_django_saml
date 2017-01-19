d39ba4e7d1c89807189fb96f53463f5265f45ec9
Patrick <patrick.hetu@gmail.com>
2017-01-19 15:36:08 -0500

    Correctif pour éviter les boucles de redirection vers /admin/login/

--- auf/django/saml/admin.py
+++ auf/django/saml/admin.py
@@ -2,10 +2,17 @@
 
 from views import redirect_to_login, redirect_to_logout
 from django.contrib.admin import site
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.conf import settings
+from django.http import HttpResponseRedirect
 
 
 def saml_login(request, extra_context=None):
-    return redirect_to_login(request)
+    if request.user.is_authenticated:
+        redirect_to = request.GET.get(REDIRECT_FIELD_NAME, settings.LOGIN_REDIRECT_URL)
+        return HttpResponseRedirect(redirect_to)
+    else:
+        return redirect_to_login(request)
 
 
 def saml_logout(request, extra_context=None):
